a1f046d5f9353e6f7817ecae733bc30ae7e78746
Frostx98 <hammoudia01@gmail.com>
2023-12-14 05:55:29 -0800

    On branch main  Your branch is ahead of 'origin/main' by 1 commit.    (use "git push" to publish your local commits)  Changes to be committed: 	modified:   Sniper.html 	modified:   Sniper.js

--- Sniper.js
+++ Sniper.js
@@ -1,4 +1,4 @@
-const correctAnswers = ['C', 'C', 'A', 'D', 'F', 'A', 'A', 'C', 'A', 'G', 'B', 'A', 'D', 'C', 'E', 'D'];
+const correctAnswers = ['C', 'C', 'A', 'D', 'F', 'A', 'A', 'C', 'A', 'G', 'B', 'A', 'D', 'C', 'E', 'D', 'B', 'A', 'C', 'A'];
 const result = document.querySelector('.result');
 const form = document.querySelector('.quiz-form');
 
@@ -6,21 +6,22 @@ form.addEventListener('submit', e => {
     e.preventDefault();
 
     let score = 0;
-    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value, 
-        form.q9.value, form.q10.value, form.q11.value, form.q12.value, form.q13.value, form.q14.value, form.q15.value, form.q16.value]
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value, form.q6.value, form.q7.value, form.q8.value,
+    form.q9.value, form.q10.value, form.q11.value, form.q12.value, form.q13.value, form.q14.value, form.q15.value, form.q16.value
+    , form.q17.value, form.q18.value, form.q19.value, form.q20.value]
     // Checking answers
 
     userAnswers.forEach((answer, index) => {
-        if(answer === correctAnswers[index]){
+        if (answer === correctAnswers[index]) {
             score += 4;
         };
     });
-    scrollTo(0,0);
+    scrollTo(0, 0);
     result.classList.remove('d-none');
     let output = 0;
     const timer = setInterval(() => {
         result.querySelector('span').textContent = `${output}%`;
-        if(output === score){
+        if (output === score) {
             clearInterval(timer);
         } else {
             output++;
